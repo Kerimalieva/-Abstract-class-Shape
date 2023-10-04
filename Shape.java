4320ee25a09780a4d78df6ffd591d598fa190e17
Kerimalieva <145113545+Kerimalieva@users.noreply.github.com>
2023-10-04 15:23:22 +0600

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,7 @@
+public abstract class Shape {    protected String name;
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    public Shape(){}    public Shape (String name){
+        this.name = name;    }
+}
